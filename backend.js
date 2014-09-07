a40d5ab6768ad67b00e4d0f23eef54b1fa36f486
Alberto Mijares <almilo@almilo.es>
2014-09-07 12:15:58 +0200

    add package.json and backend implementation

---
+++ backend.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var _ = require('lodash');
+
+var start = 0, user, app = express();
+
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/src/app'));
+
+app.get('/rest/items', function (req, res) {
+    if (user) {
+        res.send(createItems(start, 10));
+        start += 10;
+    } else {
+        res.status(401).send();
+    }
+
+    function createItems(start, number) {
+        return _.map(new Array(number), function (value, index) {
+            var id = start + index;
+            return {
+                id: id,
+                name: 'Item ' + id
+            };
+        });
+    }
+});
+
+app.post('/rest/login', function (req, res) {
+    res.send(user = 'Alberto');
+});
+
+app.post('/rest/logout', function (req, res) {
+    res.send(user = undefined);
+});
+
+app.get('/rest/user', function (req, res) {
+    res.send(user);
+});
+
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
